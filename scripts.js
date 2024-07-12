075bab79f1527ffbf78471377af6691771cf04fa
Anastasiia676 <a.polyakova13@yandex.ru>
2024-07-12 15:32:13 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+document.getElementById('advertiserForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Advertiser registered successfully!');
+    // Add your form submission code here
+});
+
+document.getElementById('partnerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Partner registered successfully!');
+    // Add your form submission code here
+});
